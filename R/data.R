bce04f7f69be37983c6e0e5caf892f246065826c
Sebastian <sebastian.daza@gmail.com>
2015-09-30 15:06:31 -0500

    update

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Data from Chile 
+#'
+#' A dataset containing regions of Chile, population, and a fake variable (proportion)
+#'
+#' @format A data frame with 15 rows and 3 variables:
+#' \describe{
+#'   \item{reg}{15 administrative regions}
+#'   \item{pob}{population}
+#'  \item{pr}{proportion of a fake variable}
+#'   ...
+#' }
+#' @source \url{http://www.ine.cl/}
+"chile"
